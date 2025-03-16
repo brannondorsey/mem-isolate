e4c311cfc43092eccbce3c1637b0084e3ca65411
Brannon Dorsey <brannon@brannondorsey.com>
2025-03-16 11:59:46 -0400

    example: Update tracing example to fallback to trace if RUST_LOG isn't set
    Also refactor to assume tracing feature is enabled as specified in
    Cargo.toml

--- examples/tracing.rs
+++ examples/tracing.rs
@@ -1,23 +1,29 @@
 // Compile error if tracing is not enabled
-#[cfg(feature = "tracing")]
-use tracing::{info, instrument};
 #[cfg(not(feature = "tracing"))]
 compile_error!(
     "This example requires the 'tracing' feature to be enabled. Run with: cargo run --example tracing --features tracing"
 );
 
 use mem_isolate::MemIsolateError;
-#[cfg_attr(feature = "tracing", instrument)]
+use tracing::{Level, info, instrument};
+use tracing_subscriber::EnvFilter;
+
+#[instrument]
 fn main() -> Result<(), MemIsolateError> {
-    #[cfg(feature = "tracing")]
-    {
-        // TODO: Make sure RUST_LOG is overriding the default
-        tracing_subscriber::fmt().with_env_filter("trace").init();
-        mem_isolate::execute_in_isolated_process(|| {
-            info!("look ma, I'm in the callable!");
-            42
-        })?;
-    }
+    init_tracing_subscriber();
+
+    mem_isolate::execute_in_isolated_process(|| {
+        info!("look ma, I'm in the callable!");
+        42
+    })?;
 
     Ok(())
+}
+
+/// Defaults to TRACE but can be overridden by the RUST_LOG environment variable
+fn init_tracing_subscriber() {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(Level::TRACE.into())
+        .from_env_lossy();
+    tracing_subscriber::fmt().with_env_filter(env_filter).init();
 }
